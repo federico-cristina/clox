8c61d2093367dfd4a9bdb25ee3c422df75bf22bd
Federico Cristina <federico.cristina@outlook.it>
2024-11-02 17:09:39 +0100

    Add buffer.h and buffer.c

---
+++ include/clox/vm/buffer.h
@@ -0,0 +1,48 @@
+#pragma once
+
+/**
+ * @file        buffer.h
+ *
+ * @author      Federico Cristina <federico.cristina@outlook.it>
+ *
+ * @copyright   Copyright (c) 2024 Federico Cristina
+ *
+ *              This file is part of the clox programming language project,
+ *              under the MIT License. See repo's LICENSE file for license
+ *              informations.
+ */
+
+#ifndef CLOX_VM_BUFFER_H_
+#define CLOX_VM_BUFFER_H_
+
+#include "clox/base/api.h"
+#include "clox/base/bits.h"
+#include "clox/base/byte.h"
+#include "clox/base/errno.h"
+
+CLOX_C_HEADER_BEGIN
+
+typedef struct _CloxBuffer
+{
+    byte_t *array;
+    size_t  count;
+    size_t  capacity;
+} CloxBuffer_t;
+
+CLOX_API CloxBuffer_t *CLOX_STDCALL cloxCreateBuffer(size_t capacity);
+
+CLOX_API CloxBuffer_t *CLOX_STDCALL cloxBufferResize(CloxBuffer_t *const buffer, size_t newCapacity);
+CLOX_API CloxBuffer_t *CLOX_STDCALL cloxBufferExpand(CloxBuffer_t *const buffer, uint32_t offset);
+CLOX_API CloxBuffer_t *CLOX_STDCALL cloxBufferShrink(CloxBuffer_t *const buffer, uint32_t offset);
+
+CLOX_API CloxBuffer_t *CLOX_STDCALL cloxBufferPush(CloxBuffer_t *const buffer, byte_t value);
+
+CLOX_API byte_t CLOX_STDCALL cloxBufferTop(const CloxBuffer_t *const buffer);
+CLOX_API byte_t CLOX_STDCALL cloxBufferPop(CloxBuffer_t *const buffer);
+CLOX_API byte_t CLOX_STDCALL cloxBufferGet(CloxBuffer_t *const buffer, size_t index);
+
+CLOX_API void CLOX_STDCALL cloxDeleteBuffer(CloxBuffer_t *const buffer);
+
+CLOX_C_HEADER_END
+
+#endif /* CLOX_VM_BUFFER_H_ */
---
+++ lib/vm/buffer.c
@@ -0,0 +1,187 @@
+/**
+ * This file is part of the clox programming language project,
+ * under the MIT License. See repo's LICENSE file for license
+ * informations.
+ */
+
+#include "clox/base/alloc.h"
+
+#include "clox/vm/buffer.h"
+
+#ifndef clox_AdjustBufferCapacity
+#   define clox_AdjustBufferCapacity(capacity) alignto(capacity, CLOX_SIZEOF_WORD_PTR)
+#endif
+
+CLOX_INLINE CloxBuffer_t *CLOX_STDCALL clox_InitBuffer(CloxBuffer_t *const buffer, size_t capacity)
+{
+    if (capacity)
+    {
+        capacity = clox_AdjustBufferCapacity(capacity);
+
+        buffer->array = dim(byte_t, capacity);
+        buffer->count = 0;
+        buffer->capacity = capacity;
+    }
+    else
+    {
+        buffer->array = NULL;
+        buffer->count = 0;
+        buffer->capacity = 0;
+    }
+
+    return buffer;
+}
+
+CLOX_API CloxBuffer_t *CLOX_STDCALL cloxCreateBuffer(size_t capacity)
+{
+    return clox_InitBuffer(alloc(CloxBuffer_t), capacity);
+}
+
+CLOX_API CloxBuffer_t *CLOX_STDCALL cloxBufferResize(CloxBuffer_t *const buffer, size_t newCapacity)
+{
+    assert(buffer != NULL);
+
+    if (!newCapacity)
+    {
+        dealloc(buffer->array);
+
+        buffer->count = 0;
+        buffer->capacity = 0;
+    }
+    else
+    {
+        newCapacity = clox_AdjustBufferCapacity(newCapacity);
+
+        if (newCapacity != buffer->capacity)
+        {
+            byte_t *newArray;
+
+            if (buffer->array)
+            {
+                newArray = (byte_t *)realloc(buffer->array, newCapacity);
+
+                if (newArray)
+                    buffer->array = newArray;
+                else
+                    failno("cannot reallocate memory");
+
+                if (buffer->count > newCapacity)
+                    buffer->count = newCapacity - 1;
+
+                buffer->capacity = newCapacity;
+            }
+            else
+            {
+                clox_InitBuffer(buffer, newCapacity);
+            }
+        }
+    }
+
+    return buffer;
+}
+
+CLOX_API CloxBuffer_t *CLOX_STDCALL cloxBufferExpand(CloxBuffer_t *const buffer, uint32_t offset)
+{
+    assert(buffer != NULL);
+
+    CloxBuffer_t *result;
+
+    if (buffer->capacity < SIZE_MAX)
+        result = cloxBufferResize(buffer, buffer->capacity + offset);
+    else
+        fail("buffer overflow", NULL);
+
+    return result;
+}
+
+CLOX_API CloxBuffer_t *CLOX_STDCALL cloxBufferShrink(CloxBuffer_t *const buffer, uint32_t offset)
+{
+    assert(buffer != NULL);
+
+    CloxBuffer_t *result;
+
+    if (buffer->capacity > 0)
+        result = cloxBufferResize(buffer, buffer->capacity - offset);
+    else
+        fail("buffer underflow", NULL);
+
+    return result;
+}
+
+CLOX_INLINE CloxBuffer_t *CLOX_STDCALL clox_BufferGrow(CloxBuffer_t *const buffer)
+{
+    return cloxBufferExpand(buffer, CLOX_SIZEOF_WORD_PTR);
+}
+
+CLOX_API CloxBuffer_t *CLOX_STDCALL cloxBufferPush(CloxBuffer_t *const buffer, byte_t value)
+{
+    assert(buffer != NULL);
+
+    if (buffer->count >= buffer->capacity)
+        clox_BufferGrow(buffer);
+
+    buffer->array[buffer->count++] = value;
+
+    return buffer;
+}
+
+CLOX_API byte_t CLOX_STDCALL cloxBufferTop(const CloxBuffer_t *const buffer)
+{
+    assert(buffer != NULL);
+
+    CLOX_REGISTER byte_t result;
+
+    if (buffer->count > 0)
+        result = buffer->array[buffer->count - 1];
+    else
+        fail("buffer underflow", NULL);
+
+    return result;
+}
+
+CLOX_API byte_t CLOX_STDCALL cloxBufferPop(CloxBuffer_t *const buffer)
+{
+    assert(buffer != NULL);
+
+    CLOX_REGISTER byte_t result;
+
+    if (buffer->count > 0)
+    {
+        result = buffer->array[--buffer->count];
+
+#ifdef CLOX_BUFFER_AUTO_SHRINK
+        if (buffer->count < (buffer->capacity - (2 * CLOX_SIZEOF_WORD_PTR)))
+            cloxBufferShrink(buffer, CLOX_SIZEOF_WORD_PTR);
+#endif
+    }
+    else
+    {
+        fail("buffer underflow", NULL);
+    }
+
+    return result;
+}
+
+CLOX_API byte_t CLOX_STDCALL cloxBufferGet(CloxBuffer_t *const buffer, size_t index)
+{
+    assert(buffer != NULL);
+
+    CLOX_REGISTER byte_t result;
+
+    if (index < buffer->count)
+        result = buffer->array[index];
+    else
+        fail("index out of bounds", NULL);
+
+    return result;
+}
+
+CLOX_API void CLOX_STDCALL cloxDeleteBuffer(CloxBuffer_t *const buffer)
+{
+    assert(buffer != NULL);
+
+    free(buffer->array);
+    free(buffer);
+
+    return;
+}
