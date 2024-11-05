2ab3ff7938bbb6bd4f79fa6d590aabebbe51000e
federico-cristina <federico.cristina@outlook.it>
2024-11-05 19:43:12 +0100

    Update test_chunk.c

--- units/vm/test_chunk.c
+++ units/vm/test_chunk.c
@@ -6,6 +6,31 @@ int main()
 {
     CloxChunk_t *b = cloxCreateChunk(0);
 
+    byte_t a[] = {
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        13,
+        14,
+        15,
+        16,
+        17,
+        18,
+        19,
+        20
+    };
+
+    cloxChunkWrite(b, a, countof(a));
+
     cloxChunkPush(b, 1);
     cloxChunkPush(b, 2);
     cloxChunkPush(b, 3);
