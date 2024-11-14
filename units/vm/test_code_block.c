e855019b9b2097c2ee889edc839a0f7fe875bc71
Federico Cristina <federico.cristina@outlook.it>
2024-11-14 18:46:24 +0100

    Update test_code_block.c

--- units/vm/test_code_block.c
+++ units/vm/test_code_block.c
@@ -5,10 +5,12 @@ int main()
 {
     byte_t program[] = {
         CLOX_OP_CODE_NOP,
-        CLOX_OP_CODE_LDBYTE,
+        CLOX_OP_CODE_LDB,
         54,
-        CLOX_OP_CODE_LDBYTE,
+        CLOX_OP_CODE_LDL,
         64,
+        32,
+        48
     };
 
     CloxCodeBlock_t block;
